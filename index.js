34b9fff3227c5591327e7297976743672ec3646d
Dmitriy <khaitdmitriy@gmail.com>
2022-03-06 21:20:50 -0500

    commit tree 10296798a732507825893fd590b67471fb5425bd

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+var main = document.querySelector("#main");
+let newHeader = document.getElementById("victory");
+
+main.remove();
+newHeader.textContent = "Ruslan is the champion"
